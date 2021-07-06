8d4eea30bca7d89e3412b1901f651f44b69c3054
Matthew-Ruczak <matthewruczak@gmail.com>
2021-07-06 05:16:55 -0500

    Created Basic UI

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -9,8 +9,90 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Spacer()
+                Text("000")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.trailing)
+                    .padding(.bottom, 20.0)
+                    .frame(width: 300.0)
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 5)
+                Spacer()
+            }
+            HStack {
+                VStack {
+                    Button("7", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    Button("4", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    Button("1", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                }
+                VStack {
+                    Button("8", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    Button("5", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    Button("2", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    Button("0", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                }
+                .padding(.top, 85.0)
+                VStack {
+                    Button("9", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    Button("6", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    Button("3", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                }
+                VStack {
+                    Button("Clear", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    Button("/", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    Button("X", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    Button("+", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                    Button("-", action: {})
+                        .padding(.bottom)
+                        .frame(width: 75.0, height: 75.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                }
+            }
+        }
+        
     }
 }
 
